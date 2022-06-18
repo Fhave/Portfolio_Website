d3c8e2cdaa579e20724b2ff5ade8708adb72d383
Fhave <omosiyobo@gmail.com>
2022-06-18 21:41:47 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, 'public', req.url == '/' ? 'index.html' : req.url);
+  let contentType = getContentType(filePath) || 'text/html';
+  let emptyPagePath = path.join(__dirname, 'public', '404.html')
+  fs.readFile(filePath, 'utf8', (err, content) => {
+    if(err){
+      if(err.code == 'ENOENT'){
+        fs.readFile(emptyPagePath, 'utf8', (err,content) => {
+          res.writeHead(200, {'ContentType' : contentType})
+          res.end(content)
+        })
+      } else {
+        res.writeHead(500);
+        res.end('Server Error')
+      }
+    }
+    if(!err){
+      res.writeHead(200, {'ContentType' : contentType})
+      res.end(content)
+    }
+  })
+})
+
+const getContentType = (filePath) => {
+  let extname = path.extname(filePath);
+  if(extname == '.js'){
+    return 'text/javascript'
+  }
+  if(extname == '.css'){
+    return 'text/css'
+  }
+  if(extname == '.png'){
+    return 'image/png'
+  }
+  if(extname == '.json'){
+    return 'application/json'
+  }
+}
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
